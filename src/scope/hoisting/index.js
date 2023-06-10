672c6feeb3609de4c17b4108e7e268d3c85aa46f
Andresguc <Andresguc@gmail.com>
2023-06-09 21:06:25 -0500

    Learnign about Hosting

---
+++ src/scope/hoisting/index.js
@@ -0,0 +1,14 @@
+// console.log(nameDog) // The declaration of the variable is undefined
+// var nameDog = 'elmo';
+
+// console.log(nanmeDog);
+
+// In functions
+
+nameDog();
+function nameDog() {
+    console.log(`The best dog in the world is ${elmo}`);
+}
+
+var elmo = "elmito";
+
